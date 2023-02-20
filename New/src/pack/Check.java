7528538e47e20c7ebd6a68d3194f16321c67ac91
rajesh <rajesh@rajesh-Lenovo-E41-25>
2023-02-20 23:34:51 +0530

    First Commit

--- New/src/pack/Check.java
+++ New/src/pack/Check.java
@@ -1,9 +1,10 @@
 package pack;
 
-public class CHeck {
+public class Check {
 
 	public static void main(String[] args) {
 		System.out.println("Hi Git");
+        System.out.println("Hi");
         System.out.println("Hi");
         System.out.println("Hi");
 	}
